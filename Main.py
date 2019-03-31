a66674819812f5696ea2b8f2793b1a7de5b83e98
karmelowsky <piotrkammer@gmail.com>
2019-03-31 11:27:46 +0200

    metrics

--- Main.py
+++ Main.py
@@ -1,16 +1,13 @@
-from sklearn.neighbors import NearestNeighbors
-from sklearn.model_selection import train_test_split
-import numpy as np
-from myFunctions import crossValidation
-from scipy.stats.stats import pearsonr
 from operator import itemgetter
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.metrics import accuracy_score
-from sklearn.model_selection import cross_val_score
-from sklearn.model_selection import cross_val_predict
 
-###########################
-##GETTING DATA#####
+import numpy as np
+from scipy.stats.stats import pearsonr
+
+from myFunctions import crossValidation
+from myFunctions import crossValidationWithScaling
+
+#region GetData
+
 with open('i.txt') as f:
     lines = f.readlines()
 
@@ -29,17 +26,17 @@ for fl in formattedList:
 
 npArray = np.array(converted)
 
-#######################################
-#
+#endregion
 
+#region GetBestFeatures
 correlations = []
 correlations2 = []
 
 for i in range(6):
-    corr, ncorr = pearsonr(npArray[:, i], npArray[:, 6])
+    corr, _ = pearsonr(npArray[:, i], npArray[:, 6])
     correlations.append([i, corr.__abs__()])
 
-    corr, ncorr = pearsonr(npArray[:, i], npArray[:, 7])
+    corr, _ = pearsonr(npArray[:, i], npArray[:, 7])
     correlations2.append([i, corr.__abs__()])
 
 npCorrelations = np.array(correlations)
@@ -54,6 +51,8 @@ print(np.array(sortedCorrelations2))
 
 bestAttributes1 = np.ravel(np.array(sortedCorrelations)[:, 0]).astype(int)
 bestAttributes2 = np.ravel(np.array(sortedCorrelations2)[:, 0]).astype(int)
+#endregion
+
 
 ###################KNN
 print("\n###ALGORYTM KNN###\n")
@@ -61,13 +60,40 @@ print("\n###ALGORYTM KNN###\n")
 y = npArray[:, 6]
 
 print("Zapalenie pecherza moczowego poprawnosc klasyfikacji:")
+
+print("Dane nieznormalizowane")
+#region NonNormalized
+print("\nMetryka euclidean")
 for k in [1, 5, 10]:
     print("\n{}-NN".format(k))
     for i in range(0, 6):
         X = npArray[:, bestAttributes1[0:i + 1]]
         print("Liczba cech: {}. Jakosc klasyfikacji: {}".format(i + 1, crossValidation(X, y, count=5, kneigbors=k)))
 
+print("\nMetryka manhattan")
+for k in [1, 5, 10]:
+    print("\n{}-NN".format(k))
+    for i in range(0, 6):
+        X = npArray[:, bestAttributes1[0:i + 1]]
+        print("Liczba cech: {}. Jakosc klasyfikacji: {}".format(i + 1, crossValidation(X, y, count=5, kneigbors=k, metric='manhattan')))
+#endregion
 
+print("Dane znormalizowane:")
+#region Normalized
+print("\nMetryka euclidean")
+for k in [1, 5, 10]:
+    print("\n{}-NN".format(k))
+    for i in range(0, 6):
+        X = npArray[:, bestAttributes1[0:i + 1]]
+        print("Liczba cech: {}. Jakosc klasyfikacji: {}".format(i + 1, crossValidationWithScaling(X, y, count=5, kneigbors=k)))
+
+print("\nMetryka manhattan")
+for k in [1, 5, 10]:
+    print("\n{}-NN".format(k))
+    for i in range(0, 6):
+        X = npArray[:, bestAttributes1[0:i + 1]]
+        print("Liczba cech: {}. Jakosc klasyfikacji: {}".format(i + 1, crossValidationWithScaling(X, y, count=5, kneigbors=k, metric='manhattan')))
+#endregion
 
 
 ###################klasa 2
@@ -75,11 +101,22 @@ y = npArray[:, 7]
 
 print("\n")
 print("Zapalenie nerek pochodzenia miedniczek nerkowych poprawnosc klasyfikacji:\n")
+
+print("\nMetryka euclidean")
+
 for k in [1, 5, 10]:
     print("\n{}-NN".format(k))
     for i in range(0, 6):
         X = npArray[:, bestAttributes2[0:i + 1]]
         print("Liczba cech: {}. Jakosc klasyfikacji: {}".format(i + 1, crossValidation(X, y, count=5, kneigbors=k)))
+
+print("\nMetryka manhattan")
+
+for k in [1, 5, 10]:
+    print("\n{}-NN".format(k))
+    for i in range(0, 6):
+        X = npArray[:, bestAttributes2[0:i + 1]]
+        print("Liczba cech: {}. Jakosc klasyfikacji: {}".format(i + 1, crossValidation(X, y, count=5, kneigbors=k, metric='manhattan')))
 
 ###################NM
 print("\n###ALGORYTM NM###\n")
--- myFunctions.py
+++ myFunctions.py
@@ -1,28 +1,59 @@
-from sklearn.neighbors import NearestNeighbors
-from sklearn.model_selection import train_test_split
-import numpy as np
-from scipy.stats.stats import pearsonr
-from operator import itemgetter
-from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score
-from sklearn.model_selection import cross_val_score
-from sklearn.model_selection import cross_val_predict
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn import preprocessing
 
-def crossValidation(X, y, count, kneigbors = 3, metric = 'euclidean'):
+def cv2(X_train, X_test, y_train, y_test, kneighbors, metric = 'euclidean'):
+
+    knn = KNeighborsClassifier(n_neighbors=kneighbors, metric=metric)
+    knn.fit(X_train, y_train)
+    pred = knn.predict(X_test)
+    score1 = accuracy_score(y_test, pred)
+
+    knn = KNeighborsClassifier(n_neighbors=kneighbors, metric=metric)
+    knn.fit(X_test, y_test)
+    pred = knn.predict(X_train)
+    score2 = accuracy_score(y_train, pred)
+
+    return (score2+score1)/2
+
+def crossValidation(X_train, X_test, y_train, y_test, count, kneigbors=3, metric='euclidean'):
     scores = []
     for i in range(0, count):
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5)
 
-        knn = KNeighborsClassifier(n_neighbors=kneigbors)
+        knn = KNeighborsClassifier(n_neighbors=kneigbors, metric=metric)
         knn.fit(X_train, y_train)
         pred = knn.predict(X_test)
         score1 = accuracy_score(y_test, pred)
         scores.append(score1)
 
-        knn = KNeighborsClassifier(n_neighbors=kneigbors)
+        knn = KNeighborsClassifier(n_neighbors=kneigbors, metric=metric)
         knn.fit(X_test, y_test)
         pred = knn.predict(X_train)
         score2 = accuracy_score(y_train, pred)
         scores.append(score2)
 
-    return sum(scores)/len(scores)
+    return sum(scores) / len(scores)
+
+
+def crossValidationWithScaling(X, y, count, kneigbors=3, metric='euclidean'):
+    scores = []
+    for i in range(0, 100):
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5)
+
+        scaledX = preprocessing.scale(X_train)
+        knn = KNeighborsClassifier(n_neighbors=kneigbors, metric=metric)
+        knn.fit(scaledX, y_train)
+        pred = knn.predict(X_test)
+        score1 = accuracy_score(y_test, pred)
+        scores.append(score1)
+
+        scaledX = preprocessing.scale(X_test)
+        knn = KNeighborsClassifier(n_neighbors=kneigbors, metric=metric)
+        knn.fit(scaledX, y_test)
+        pred = knn.predict(X_train)
+        score2 = accuracy_score(y_train, pred)
+        scores.append(score2)
+
+    return sum(scores) / len(scores)
